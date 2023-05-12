6dbc7589c19db1144adf79260c02d12082b6a697
菲利 <stephanovallejom@duck.com>
2023-05-12 11:19:08 -0500

    Update system.sql

--- system.sql
+++ system.sql
@@ -2,13 +2,13 @@ select * from tab;
 
 create tablespace DATOS
 datafile '/opt/oracle/oradata/XE/XEPDB1/DATOS.dbf'
-size 1M
-    autoextend on next 2M;
+size 500M
+    autoextend on next 100M;
     
 create tablespace INDICES
 datafile '/opt/oracle/oradata/XE/XEPDB1/INDICES.dbf'
-size 1M
-    autoextend on next 2M;
+size 500M
+    autoextend on next 100M;
     
 alter session set "_ORACLE_SCRIPT"=TRUE;
 create user sisfactura identified by lticPUCE default tablespace DATOS;
