84bb49161601359319bba8cdcd43d86d185a1d18
菲利 <stephanovallejom@duck.com>
2023-05-15 10:20:37 -0500

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -16,3 +16,7 @@ drop user usuario cascade;
 alter session set "_ORACLE_SCRIPT"=TRUE;
 create role facturador;
 grant select on sisfacturacion.bodega to facturador;
+grant select, insert on sisfacturacion.cliente to facturador;
+grant select, update on sisfacturacion.producto to facturador;
+grant select, insert, update, delete on sisfacturacion.factura to facturador;
+grant select, insert, update, delete on sisfacturacion.detalle_factura to facturador;
