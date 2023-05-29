ff94ffae81a56378563f72975dee0f5e44022f27
菲利 <stephanovallejom@duck.com>
2023-05-29 10:04:52 -0500

    Update scripts5.sql

--- scripts5.sql
+++ scripts5.sql
@@ -9,29 +9,66 @@ create table PRODUCTOAUX (
     tablespace DATOS
 /
 
-/*
-crear un disparador al momento de crear un nuevo registro y que actualize el AUX_CODIGO y el AUX_PRECIO
-el aux_codigo se estrctura a_b_c_dd_ee
-a corresponde a aux_tipo que puede ser 1 al 3
-b corresponde a aux_modelo que puede ser 1 al 5
-c corresponde a aux_color que puede ser 1 al 4, 1 = blanco, 2 = azul, 3 = negro, 4 = rojo
-dd corresponde a aux_talla que puede ser del 3 al 45
-ee corresponde a aux_precio
-aux_precio se calcula asi
-el precio de un zapato es 20 dolares
-si es color negro se suma 5%
-si es color blanco se suma 10%
-si es color azul se suma 20%
-si es color rojo se suma 30%
-ademas se suma la talla
-si aux_talla es 3 al 15 se suma 5%
-si aux_talla es 16 al 30 se suma 10%
-si aux_talla es de 31 al 40 se suma 15%
-aux_talla mayores a 40 se suma 20%
-*/
--- el trigger se llama DISPARADORAUX
-create or replace trigger DISPARADORAUX
+desc PRODUCTOAUX;
+
+-- disparador al momento de crear un nuevo registro y que actualize el AUX_CODIGO y el AUX_PRECIO
+-- aux_codigo se estrctura a_b_c_dd_ee
+create or replace trigger TRG_PRODUCTOAUX
 before insert on PRODUCTOAUX
 for each row
+-- a = aux_tipo del 1 al 3
+-- b = aux_modelo del 1 al 5
+-- c = aux_color del 1 al 4
+-- dd = aux_talla del 3 al 45
+-- ee = aux_precio
+-- usar los valores insertados para actualizar aux_codigo y aux_precio
 begin
-    if 
+-- restringir a, b, c, dd, ee si los valores no estan dentro del rango
+    if :new.AUX_TIPO not between 1 and 3 then
+        raise_application_error(-20001, 'El valor de AUX_TIPO no esta dentro del rango');
+    end if;
+    if :new.AUX_MODELO not between 1 and 5 then
+        raise_application_error(-20002, 'El valor de AUX_MODELO no esta dentro del rango');
+    end if;
+    if :new.AUX_COLOR not between 1 and 4 then
+        raise_application_error(-20003, 'El valor de AUX_COLOR no esta dentro del rango');
+    end if;
+    if :new.AUX_TALLA not between 3 and 45 then
+        raise_application_error(-20004, 'El valor de AUX_TALLA no esta dentro del rango');
+    end if;
+
+    -- actializar aux_precio
+    -- precio de zapato es 20
+    -- si aux_color = 3 se suma 5%
+    -- si aux_color = 1 se suma 10%
+    -- si aux_color = 2 se suma 20%
+    -- si aux_color = 4 se suma 30%
+    if :new.AUX_COLOR = 3 then
+        :new.AUX_PRECIO := :new.AUX_PRECIO + (:new.AUX_PRECIO * 0.05);
+    elsif :new.AUX_COLOR = 1 then
+        :new.AUX_PRECIO := :new.AUX_PRECIO + (:new.AUX_PRECIO * 0.1);
+    elsif :new.AUX_COLOR = 2 then
+        :new.AUX_PRECIO := :new.AUX_PRECIO + (:new.AUX_PRECIO * 0.2);
+    elsif :new.AUX_COLOR = 4 then
+        :new.AUX_PRECIO := :new.AUX_PRECIO + (:new.AUX_PRECIO * 0.3);
+    end if;
+    -- si aux_talla 3 a 15 se suma 5%
+    -- si aux_talla 16 a 30 se suma 10%
+    -- si aux_talla 31 a 40 se suma 15%
+    -- si aux_talla > 40 se suma 20%
+    if :new.AUX_TALLA between 3 and 15 then
+        :new.AUX_PRECIO := :new.AUX_PRECIO + (:new.AUX_PRECIO * 0.05);
+    elsif :new.AUX_TALLA between 16 and 30 then
+        :new.AUX_PRECIO := :new.AUX_PRECIO + (:new.AUX_PRECIO * 0.1);
+    elsif :new.AUX_TALLA between 31 and 40 then
+        :new.AUX_PRECIO := :new.AUX_PRECIO + (:new.AUX_PRECIO * 0.15);
+    elsif :new.AUX_TALLA > 40 then
+        :new.AUX_PRECIO := :new.AUX_PRECIO + (:new.AUX_PRECIO * 0.2);
+    end if;
+
+    -- actualizar aux_codigo
+    :new.AUX_CODIGO := :new.AUX_TIPO || '_' || :new.AUX_MODELO || '_' || :new.AUX_COLOR || '_' || :new.AUX_TALLA || '_' || :new.AUX_PRECIO;
+end TRG_PRODUCTOAUX;
+/
+
+insert into PRODUCTOAUX values ('3', '1', '2', '3', '19', 20.0);
