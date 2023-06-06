255f02b71275ad5edc1ecbec3f5217e60b412fa2
菲利 <stephanovallejom@duck.com>
2023-06-05 23:59:26 -0500

    Update insert.sql

--- proy/insert.sql
+++ proy/insert.sql
@@ -97,3 +97,45 @@ create table REVISTA (
    constraint PK_REVISTA primary key (REV_ID)
 )
 
+-- 6 atri
+desc congreso; 
+-- tipo nacional o internacional
+insert into congreso values ('1', 'con1', 'nacional',
+
+desc descriptor; 
+-- 2 atri
+
+desc descriptor_linea; 
+-- 2 atri
+
+desc investigacion; 
+-- 8 atri
+
+desc linea_investigacion;
+-- 3
+
+desc profesor; 
+-- 7
+
+desc profesor_investigacion; 
+-- 5
+
+desc profesor_linea; 
+-- 2
+
+desc profesor_publicacion; 
+-- 2
+
+desc publicacion; 
+-- 3
+
+desc publicacion_congreso; 
+-- 2
+
+desc publicacion_revista; 
+-- 4
+
+desc revista;
+-- 5
+insert into revista values ('1', 'c1', 'ed1', 1, 1);
+insert into revista values ('2', 'c1', 'ed1', 1, 1);
