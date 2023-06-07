02d53bfd462070c73053d36fbee633fa0561e609
菲利 <stephanovallejom@duck.com>
2023-06-06 23:42:00 -0500

    Update triggers.sql

--- proy/triggers.sql
+++ proy/triggers.sql
@@ -1,1 +1,10 @@
--- triggers
+-- trigger que unicamente permita que el tipo de congreso sea nacional o internacional
+create or replace trigger tipo_congreso
+before insert or update on congreso
+for each row
+begin
+    if :new.CON_TIPO not in ('Nacional', 'Internacional') then
+        raise_application_error(-20000, 'El tipo de congreso debe ser nacional o internacional');
+    end if;
+end;
+/
