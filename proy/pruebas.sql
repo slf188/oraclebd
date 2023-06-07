2daa740768def7abf6d143a80d541ca19a27be25
菲利 <stephanovallejom@duck.com>
2023-06-07 00:18:18 -0500

    Update pruebas.sql

--- proy/pruebas.sql
+++ proy/pruebas.sql
@@ -3,3 +3,10 @@ insert into congreso values ('CON-3', 'Congreso 3', 'Otro', to_date('01/01/2023'
 
 -- trigger paginas_revista
 insert into revista values ('REV-3', 'Revista 4', 'Editorial 4', 4, 4, 8, 6);
+
+-- trigger lider_investigacion
+-- no pasa
+insert into profesor_investigacion values ('PRF-3', 'INV-1', to_date('05/01/2023', 'dd/mm/yyyy'), to_date('06/01/2023', 'dd/mm/yyyy'), 1);
+-- pasa
+insert into profesor_investigacion values ('PRF-3', 'INV-1', to_date('05/01/2023', 'dd/mm/yyyy'), to_date('06/01/2023', 'dd/mm/yyyy'), 0);
+
