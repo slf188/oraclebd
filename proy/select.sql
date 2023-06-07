1dd9ad815c9b1e29d739b7e62242b31b9d4c8cca
菲利 <stephanovallejom@duck.com>
2023-06-07 17:43:57 -0500

    Create select.sql

---
+++ proy/select.sql
@@ -0,0 +1,12 @@
+-- selects
+select * from congreso;
+select * from descriptor;
+select * from investigacion;
+select * from publicacion;
+select * from linea_investigacion;
+select * from descriptor_linea;
+select * from supervisor;
+select * from investigacion_supervisor;
+select * from profesor;
+select * from profesor_investigacion;
+select * from revista; 
