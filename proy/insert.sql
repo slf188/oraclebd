6cab15d76b042b16b5e46461496ce6be344953aa
菲利 <stephanovallejom@duck.com>
2023-06-06 23:29:14 -0500

    Update insert.sql

--- proy/insert.sql
+++ proy/insert.sql
@@ -33,6 +33,11 @@ create table INVESTIGACION (
    constraint PK_INVESTIGACION primary key (INV_ID)
 )
 
+create table INVESTIGACION_SUPERVISOR (
+   SUP_ID               VARCHAR2(8)           not null,
+   INV_ID               VARCHAR2(8)           not null
+)
+
 create table LINEA_INVESTIGACION (
    LNINV_ID             VARCHAR2(8)           not null,
    PUB_ID               INTEGER               not null,
@@ -40,23 +45,31 @@ create table LINEA_INVESTIGACION (
    constraint PK_LINEA_INVESTIGACION primary key (LNINV_ID)
 )
 
+create table PROFESOR (
+   PRF_ID               VARCHAR2(8)           not null,
+   PRF_NOMBRES          VARCHAR2(64),
+   PRF_APELLIDOS        VARCHAR2(64),
+   PRF_NUM_DESPACHO     INTEGER,
+   PRF_TELEFONO         VARCHAR2(15),
+   PRF_TITULACION       VARCHAR2(40),
+   constraint PK_PROFESOR primary key (PRF_ID)
+)
+
+create table PROFESOR_INVESTIGACION (
+   PRF_ID               VARCHAR2(8)           not null,
+   INV_ID               VARCHAR2(8)           not null,
+   PRFINV_FECHA_INICIO  DATE,
+   PRFINV_FECHA_FIN     DATE,
+   PRFINV_ES_LIDER      NUMBER
+)
+
 create table PUBLICACION (
    PUB_ID               INTEGER               not null,
+   CON_ID               VARCHAR2(8)           not null,
+   REV_ID               VARCHAR2(8)           not null,
    INV_ID               VARCHAR2(8)           not null,
    PUB_TITULO           VARCHAR2(40),
    constraint PK_PUBLICACION primary key (PUB_ID)
-)
-
-create table PUBLICACION_CONGRESO (
-   CON_ID               VARCHAR2(8)           not null,
-   PUB_ID               INTEGER               not null
-)
-
-create table PUBLICACION_REVISTA (
-   PUB_ID               INTEGER               not null,
-   REV_ID               VARCHAR2(8)           not null,
-   PUBREV_PAG_INICIO    INTEGER,
-   PUBREV_PAG_FIN       INTEGER
 )
 
 create table REVISTA (
@@ -65,62 +78,63 @@ create table REVISTA (
    REV_EDITORIAL        VARCHAR2(40),
    REV_VOLUMEN          INTEGER,
    REV_NUMERO           INTEGER,
+   REV_PAG_INICIO       INTEGER,
+   REV_PAG_FIN          INTEGER,
    constraint PK_REVISTA primary key (REV_ID)
 )
 
--- 6 atri
-desc congreso; 
--- tipo nacional o internacional
-insert into congreso values ('1', 'con1', 'nacional', to_date('06/06/2022', 'dd/mm/yyyy'), to_date('08/06/2023', 'dd/mm/yyyy'), 'Madrid', 'España', 'ed1');
-insert into congreso values ('2', 'con2', 'internacional', to_date('05/05/2022', 'dd/mm/yyyy'), to_date('05/05/2023', 'dd/mm/yyyy'), 'Valencia', 'España', 'ed2');
+create table SUPERVISOR (
+   SUP_ID               VARCHAR2(8)           not null,
+   SUP_NOMBRE           VARCHAR2(64),
+   constraint PK_SUPERVISOR primary key (SUP_ID)
+)
 
-desc descriptor; 
--- 2 atri
-insert into descriptor values ('1', 'desc1');
-insert into descriptor values ('2', 'desc2');
+-- 2 inserts de cada tabla
+insert into congreso values ('CON-1', 'Congreso 1', 'Nacional', to_date('01/01/2023', 'dd/mm/yyyy'), to_date('02/01/2023', 'dd/mm/yyyy'), 'Madrid', 'España', 'Editorial 1');
+insert into congreso values ('CON-2', 'Congreso 2', 'Internacional', to_date('03/01/2023', 'dd/mm/yyyy'), to_date('04/01/2023', 'dd/mm/yyyy'), 'Valencia', 'España', 'Editorial 2');
 
-desc descriptor_linea; 
--- 2 atri
-insert into descriptor_linea values ('1', '1');
-insert into descriptor_linea values ('2', '2');
+insert into descriptor values ('DESC-1', 'Descriptor 1');
+insert into descriptor values ('DESC-2', 'Descriptor 2');
 
-desc investigacion; 
--- 8 atri
-insert into investigacion values ('1', 'inv1', 'acr1', 'desc1', 'fin1', 1000, to_date('01/01/2022', 'dd/mm/yyyy'), to_date('01/01/2023', 'dd/mm/yyyy'));
-insert into investigacion values ('2', 'inv2', 'acr2', 'desc2', 'fin2', 2000, to_date('02/02/2022', 'dd/mm/yyyy'), to_date('02/02/2023', 'dd/mm/yyyy'));
+insert into investigacion values ('INV-1', 'Investigacion 1', 'INV1', 'Descripcion 1', 'Financiador 1', 1000, to_date('01/01/2023', 'dd/mm/yyyy'), to_date('02/01/2023', 'dd/mm/yyyy'));
+insert into investigacion values ('INV-2', 'Investigacion 2', 'INV2', 'Descripcion 2', 'Financiador 2', 2000, to_date('03/01/2023', 'dd/mm/yyyy'), to_date('04/01/2023', 'dd/mm/yyyy'));
+
+insert into publicacion values (1, 'CON-1', 'REV-1', 'INV-1', 'Titulo 1');
+insert into publicacion values (2, 'CON-2', 'REV-2', 'INV-2', 'Titulo 2');
+
+insert into linea_investigacion values ('LNINV-1', 1, 'Linea 1');
+insert into linea_investigacion values ('LNINV-2', 2, 'Linea 2');
 
 desc linea_investigacion;
--- 3
-insert into linea_investigacion values ('1', 1, 'lin1');
-insert into linea_investigacion values ('2', 2, 'lin2');
 
-desc publicacion; 
--- 3
-insert into publicacion values (1, '1', 'pub1');
-insert into publicacion values (2, '2', 'pub2');
+insert into descriptor_linea values ('DESC-1', 'LNINV-1');
+insert into descriptor_linea values ('DESC-2', 'LNINV-2');
 
-desc publicacion_congreso; 
--- 2
-insert into publicacion_congreso values ('1', 1);
-insert into publicacion_congreso values ('2', 2);
+insert into supervisor values ('SUP-1', 'Supervisor 1');
+insert into supervisor values ('SUP-2', 'Supervisor 2');
 
-desc publicacion_revista; 
--- 4
-insert into publicacion_revista values (1, '1', 1, 2);
-insert into publicacion_revista values (2, '2', 3, 4);
+insert into investigacion_supervisor values ('SUP-1', 'INV-1');
+insert into investigacion_supervisor values ('SUP-2', 'INV-2');
 
-desc revista;
--- 5
-insert into revista values ('1', 'rev1', 'edi1', 1, 1);
-insert into revista values ('2', 'rev2', 'edi2', 2, 2);
+insert into profesor values ('PRF-1', 'Ivan', 'Moyota', 1, '123456789', 'Matematicas');
+insert into profesor values ('PRF-2', 'Patricio', 'Santillan', 2, '987654321', 'Ciencias Sociales');
+
+insert into profesor_investigacion values ('PRF-1', 'INV-1', to_date('01/01/2023', 'dd/mm/yyyy'), to_date('02/01/2023', 'dd/mm/yyyy'), 1);
+insert into profesor_investigacion values ('PRF-2', 'INV-2', to_date('03/01/2023', 'dd/mm/yyyy'), to_date('04/01/2023', 'dd/mm/yyyy'), 0);
+
+insert into revista values ('REV-1', 'Revista 1', 'Editorial 1', 1, 1, 1, 1);
+insert into revista values ('REV-2', 'Revista 2', 'Editorial 2', 2, 2, 2, 2);
+
 
 -- selects
 select * from congreso;
 select * from descriptor;
-select * from descriptor_linea;
 select * from investigacion;
-select * from linea_investigacion;
 select * from publicacion;
-select * from publicacion_congreso; 
-select * from publicacion_revista;
+select * from linea_investigacion;
+select * from descriptor_linea;
+select * from supervisor;
+select * from investigacion_supervisor;
+select * from profesor;
+select * from profesor_investigacion;
 select * from revista; 
