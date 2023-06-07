824808f5cfd793070b5026a2e55329bbc056963f
菲利 <stephanovallejom@duck.com>
2023-06-06 23:51:05 -0500

    Update pruebas.sql

--- proy/pruebas.sql
+++ proy/pruebas.sql
@@ -1,1 +1,5 @@
+-- tipo_congreso trigger
 insert into congreso values ('CON-3', 'Congreso 3', 'Otro', to_date('01/01/2023', 'dd/mm/yyyy'), to_date('02/01/2023', 'dd/mm/yyyy'), 'Madrid', 'España', 'Editorial 1');
+
+-- trigger paginas_revista
+insert into revista values ('REV-3', 'Revista 4', 'Editorial 4', 4, 4, 8, 6);
