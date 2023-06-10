de003553eb0925d3840e6b30bf4244cebcb0182f
菲利 <stephanovallejom@duck.com>
2023-06-10 17:37:46 -0500

    Update create_tables.sql

--- proy/create_tables.sql
+++ proy/create_tables.sql
@@ -1,44 +1,17 @@
-create table CONGRESO (
-   CON_ID               VARCHAR2(8)           not null,
-   CON_NOMBRE           VARCHAR2(64),
-   CON_TIPO             VARCHAR2(30),
-   CON_FECHA_INICIO     DATE,
-   CON_FECHA_FIN        DATE,
-   CON_LUGAR_CELEBRACION VARCHAR2(50),
-   CON_PAIS             VARCHAR2(50),
-   CON_EDITORIAL        VARCHAR2(30),
-   constraint PK_CONGRESO primary key (CON_ID)
-)
-
-create table DESCRIPTOR (
-   DESC_ID              VARCHAR2(8)           not null,
-   DESC_NOMBRE          VARCHAR2(64),
-   constraint PK_DESCRIPTOR primary key (DESC_ID)
-)
-
-create table DESCRIPTOR_LINEA (
-   DESC_ID              VARCHAR2(8)           not null,
-   LNINV_ID             VARCHAR2(8)           not null
-)
-
-create table INVESTIGACION (
-   INV_ID               VARCHAR2(8)           not null,
-   SUP_ID               VARCHAR2(8),
-   INV_NOMBRE           VARCHAR2(64),
-   INV_ACRONIMO         VARCHAR2(10),
-   INV_DESCRIPCION      VARCHAR2(200),
-   INV_FINANCIADOR      VARCHAR2(64),
-   INV_PRESUPUESTO      NUMBER(12,4),
-   INV_FECHA_INICIO     DATE,
-   INV_FECHA_FIN        DATE,
-   constraint PK_INVESTIGACION primary key (INV_ID)
-)
-
 create table LINEA_INVESTIGACION (
    LNINV_ID             VARCHAR2(8)           not null,
    PUB_ID               INTEGER               not null,
    LNINV_NOMBRE         VARCHAR2(10),
+   LNINV_DESCRIPTORES   VARCHAR2(150),
    constraint PK_LINEA_INVESTIGACION primary key (LNINV_ID)
+)
+
+create table PROFESOR_INVESTIGACION (
+   PRF_ID               VARCHAR2(8)           not null,
+   INV_ID               VARCHAR2(8)           not null,
+   PRFINV_FECHA_INICIO  DATE,
+   PRFINV_FECHA_FIN     DATE,
+   PRFINV_ES_LIDER      NUMBER
 )
 
 create table PROFESOR (
@@ -49,13 +22,6 @@ create table PROFESOR (
    PRF_TELEFONO         VARCHAR2(15),
    PRF_TITULACION       VARCHAR2(40),
    constraint PK_PROFESOR primary key (PRF_ID)
-)
-
-create table PROFESOR_INVESTIGACION (
-   PRF_ID               VARCHAR2(8)           not null,
-   INV_ID               VARCHAR2(8)           not null,
-   PRFINV_FECHA_INICIO  DATE,
-   PRFINV_FECHA_FIN     DATE
 )
 
 create table PUBLICACION (
@@ -78,10 +44,32 @@ create table REVISTA (
    constraint PK_REVISTA primary key (REV_ID)
 )
 
+create table INVESTIGACION (
+   INV_ID               VARCHAR2(8)           not null,
+   SUP_ID               VARCHAR2(8),
+   INV_NOMBRE           VARCHAR2(64),
+   INV_ACRONIMO         VARCHAR2(10),
+   INV_DESCRIPCION      VARCHAR2(200),
+   INV_FINANCIADOR      VARCHAR2(64),
+   INV_PRESUPUESTO      NUMBER(12,4),
+   INV_FECHA_INICIO     DATE,
+   INV_FECHA_FIN        DATE,
+   constraint PK_INVESTIGACION primary key (INV_ID)
+)
+
+create table CONGRESO (
+   CON_ID               VARCHAR2(8)           not null,
+   CON_NOMBRE           VARCHAR2(64),
+   CON_TIPO             VARCHAR2(30),
+   CON_FECHA_INICIO     DATE,
+   CON_FECHA_FIN        DATE,
+   CON_LUGAR_CELEBRACION VARCHAR2(50),
+   CON_PAIS             VARCHAR2(50),
+   CON_EDITORIAL        VARCHAR2(30),
+   constraint PK_CONGRESO primary key (CON_ID)
+)
+
 create table SUPERVISOR (
    SUP_ID               VARCHAR2(8)           not null,
-   PRF_APELLIDOS        VARCHAR2(64),
-   SUP_NOMBRES          VARCHAR2(64),
-   SUP_TITULACION       VARCHAR(40),
    constraint PK_SUPERVISOR primary key (SUP_ID)
 )
