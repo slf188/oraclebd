4eb71a1abb145b158476974809813e9782c5b903
菲利 <stephanovallejom@duck.com>
2023-06-10 17:38:48 -0500

    Update desc.sql

--- proy/desc.sql
+++ proy/desc.sql
@@ -1,10 +1,8 @@
 -- desc
 desc congreso;
-desc descriptor;
 desc investigacion;
 desc publicacion;
 desc linea_investigacion;
-desc descriptor_linea;
 desc supervisor;
 desc profesor;
 desc profesor_investigacion;
