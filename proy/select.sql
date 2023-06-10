a0d5f2684bb369148b5f05915b0431d9d24c3c45
菲利 <stephanovallejom@duck.com>
2023-06-10 17:34:33 -0500

    Update select.sql

--- proy/select.sql
+++ proy/select.sql
@@ -1,11 +1,9 @@
 -- selects
 select * from congreso;
-select * from descriptor;
 select * from investigacion;
 select * from publicacion;
 select * from linea_investigacion;
-select * from descriptor_linea;
 select * from supervisor;
 select * from profesor;
 select * from profesor_investigacion;
-select * from revista; 
+select * from revista;
