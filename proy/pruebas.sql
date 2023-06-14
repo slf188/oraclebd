5a4512633e057073ca6fd2c1a27b58751576058b
菲利 <stephanovallejom@duck.com>
2023-06-13 23:52:25 -0500

    Update pruebas.sql

--- proy/pruebas.sql
+++ proy/pruebas.sql
@@ -40,3 +40,44 @@ update profesor_investigacion set PRFINV_ES_LIDER = 1 where PRF_ID = 'PROF_002';
 insert into profesor_investigacion values ('PROF_002', 'INVE_005', to_date('01/01/2020', 'dd/mm/yyyy'), to_date('01/01/2019', 'dd/mm/yyyy'), 0);
 -- pasa
 insert into profesor_investigacion values ('PROF_002', 'INVE_005', to_date('01/01/2019', 'dd/mm/yyyy'), to_date('01/01/2020', 'dd/mm/yyyy'), 0);
+
+-- prueba fecha_fin_profesor_investigacion
+-- no pasa
+insert into profesor_investigacion values ('PROF_001', 'INVE_003', to_date('01/01/2017', 'dd/mm/yyyy'), to_date('01/01/2020', 'dd/mm/yyyy'), 0);
+-- pasa
+insert into profesor_investigacion values ('PROF_001', 'INVE_003', to_date('01/01/2017', 'dd/mm/yyyy'), to_date('01/01/2018', 'dd/mm/yyyy'), 0);
+
+-- prueba fecha_inicio_profesor_investigacion
+-- no pasa
+insert into profesor_investigacion values ('PROF_001', 'INVE_002', to_date('01/01/2015', 'dd/mm/yyyy'), to_date('01/01/2016', 'dd/mm/yyyy'), 0);
+-- pasa
+insert into profesor_investigacion values ('PROF_001', 'INVE_002', to_date('01/01/2016', 'dd/mm/yyyy'), to_date('01/01/2017', 'dd/mm/yyyy'), 0);
+
+-- prueba pagina_inicio_fin_revista
+-- no pasa
+insert into revista values ('REVI_006', 'Revista 6', 'Editorial 6', 6, 6, 60, 60);
+-- pasa
+insert into revista values ('REVI_006', 'Revista 6', 'Editorial 6', 6, 6, 1, 61);
+
+-- prueba participacion_profesor_investigacion
+-- no pasa
+insert into profesor_investigacion values ('PROF_001', 'INVE_001', to_date('01/01/2015', 'dd/mm/yyyy'), to_date('01/01/2016', 'dd/mm/yyyy'), 0);
+
+-- prueba trg_validar_publicacion
+-- no pasa
+insert into publicacion values (6, 'INVE_006', 'REVI_006', 'CONG_003', 'Titulo 6');
+-- pasa
+insert into publicacion values (7, 'INVE_007', null, 'CONG_004', 'Titulo 7');
+
+-- prueba trg_verificar_participacion_lider
+-- no pasa
+insert into profesor_investigacion values ('PROF_004', 'INVE_003', to_date('01/01/2017', 'dd/mm/yyyy'), to_date('03/01/2017', 'dd/mm/yyyy'), 1);
+-- pasa
+insert into profesor_investigacion values ('PROF_004', 'INVE_003', to_date('01/01/2017', 'dd/mm/yyyy'), to_date('01/01/2018', 'dd/mm/yyyy'), 1);
+
+-- prueba prf_num_despacho y prf_telefono
+-- no pasa
+insert into profesor values ('PROF_006', 'Luis', 'Gonzales', 5, '123456', 'No Doctor');
+insert into profesor values ('PROF_006', 'Luis', 'Gonzales', 6, '12345', 'No Doctor');
+-- pasa
+insert into profesor values ('PROF_006', 'Luis', 'Gonzales', 6, '123456', 'No Doctor');
