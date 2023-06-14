9406fba52c0488f2c462bd9a60f32749226f0ba4
菲利 <stephanovallejom@duck.com>
2023-06-13 23:34:50 -0500

    Update pruebas.sql

--- proy/pruebas.sql
+++ proy/pruebas.sql
@@ -1,76 +1,42 @@
--- tipo_congreso trigger
+-- prueba tipo_congreso
 -- no pasa
-insert into congreso values ('CON-3', 'Congreso 3', 'Otro', to_date('01/01/2023', 'dd/mm/yyyy'), to_date('02/01/2023', 'dd/mm/yyyy'), 'Madrid', 'España', 'Editorial 3');
+insert into congreso values ('CONG_006', 'Congreso 6', 'Social', to_date('01/01/2020', 'dd/mm/yyyy'), to_date('01/01/2021', 'dd/mm/yyyy'), 'Santiago', 'Chile', 'Editorial 6');
 -- pasa
-insert into congreso values ('CON-3', 'Congreso 3', 'nacional', to_date('01/01/2023', 'dd/mm/yyyy'), to_date('02/01/2023', 'dd/mm/yyyy'), 'Barcelona', 'España', 'Editorial 3');
+insert into congreso values ('CONG_006', 'Congreso 6', 'Internacional', to_date('01/01/2020', 'dd/mm/yyyy'), to_date('01/01/2021', 'dd/mm/yyyy'), 'Santiago', 'Chile', 'Editorial 6');
 
--- trigger paginas_revista
+-- prueba paginas_revista y pagina_inicio_revista
 -- no pasa
-insert into revista values ('REV-3', 'Revista 4', 'Editorial 4', 4, 4, 8, 6);
+update revista set REV_PAG_INICIO = 10, REV_PAG_FIN = 1 where REV_ID = 'REVI_001';
 -- pasa
-insert into revista values ('REV-3', 'Revista 4', 'Editorial 4', 4, 4, 8, 16);
+update revista set REV_PAG_INICIO = 1, REV_PAG_FIN = 6 where REV_ID = 'REVI_001';
 
--- trigger lider_investigacion
+-- prueba titulacion_doctor
+select * from profesor;
 -- no pasa
-insert into profesor_investigacion values ('PRF-3', 'INV-1', to_date('05/01/2023', 'dd/mm/yyyy'), to_date('06/01/2023', 'dd/mm/yyyy'), 1);
+insert into profesor values ('PROF_006', 'Luis', 'Gonzales', 6, '123456', 'Ingeniero');
 -- pasa
-insert into profesor_investigacion values ('PRF-3', 'INV-1', to_date('05/01/2023', 'dd/mm/yyyy'), to_date('06/01/2023', 'dd/mm/yyyy'), 0);
+insert into profesor values ('PROF_006', 'Luis', 'Gonzales', 6, '123456', 'Doctor');
 
--- titulacion_doctor
+-- prueba lider_investigacion
 -- no pasa
-insert into profesor values ('PRF-4', 'Damian', 'Nicolalde', 4, '123456789', 'Otro');
+insert into profesor_investigacion values ('PROF_002', 'INVE_001', to_date('01/01/2015', 'dd/mm/yyyy'), to_date('01/01/2016', 'dd/mm/yyyy'), 1);
 -- pasa
-insert into profesor values ('PRF-4', 'Damian', 'Nicolalde', 4, '123456789', 'Doctor');
-insert into profesor values ('PRF-5', 'Henry', 'Roa', 5, '123456789', 'no doctor');
-
--- lider_doctor
--- no pasa
-insert into profesor_investigacion values ('PRF-5', 'INV-1', to_date('05/01/2023', 'dd/mm/yyyy'), to_date('06/01/2023', 'dd/mm/yyyy'), 1);
--- pasa
-insert into profesor_investigacion values ('PRF-5', 'INV-2', to_date('05/01/2023', 'dd/mm/yyyy'), to_date('06/01/2023', 'dd/mm/yyyy'), 0);
+insert into profesor_investigacion values ('PROF_003', 'INVE_001', to_date('01/01/2015', 'dd/mm/yyyy'), to_date('01/01/2016', 'dd/mm/yyyy'), 0);
 
 -- prueba fecha_investigacion
 -- no pasa
-insert into investigacion values ('INV-3', 'SUP-3' , 'Investigacion 3', 'INV3', 'Descripcion 3', 'Financiador 3', 3000, to_date('03/01/2023', 'dd/mm/yyyy'), to_date('02/01/2023', 'dd/mm/yyyy'));
+insert into investigacion values ('INVE_006', 'Investigacion 6', 'INV_6', 'Descripcion 6', 'Financiador 6', 6000, to_date('01/01/2020', 'dd/mm/yyyy'), to_date('01/01/2019', 'dd/mm/yyyy'));
 -- pasa
-insert into investigacion values ('INV-3', 'SUP-3', 'Investigacion 3', 'INV3', 'Descripcion 3', 'Financiador 3', 3000, to_date('01/01/2023', 'dd/mm/yyyy'), to_date('03/01/2023', 'dd/mm/yyyy'));
+insert into investigacion values ('INVE_007', 'Investigacion 7', 'INV_7', 'Descripcion 7', 'Financiador 7', 7000, to_date('01/01/2018', 'dd/mm/yyyy'), to_date('01/01/2019', 'dd/mm/yyyy'));
+
+-- prueba lider_doctor
+-- no pasa
+insert into profesor_investigacion values ('PROF_005', 'INVE_002', to_date('01/01/2016', 'dd/mm/yyyy'), to_date('01/01/2017', 'dd/mm/yyyy'), 1);
+-- pasa
+update profesor_investigacion set PRFINV_ES_LIDER = 1 where PRF_ID = 'PROF_002';
 
 -- prueba fecha_profesor_investigacion
 -- no pasa
-insert into profesor_investigacion values ('PRF-4', 'INV-1', to_date('05/01/2025', 'dd/mm/yyyy'), to_date('04/01/2023', 'dd/mm/yyyy'), 0);
+insert into profesor_investigacion values ('PROF_002', 'INVE_005', to_date('01/01/2020', 'dd/mm/yyyy'), to_date('01/01/2019', 'dd/mm/yyyy'), 0);
 -- pasa
-insert into profesor_investigacion values ('PRF-4', 'INV-1', to_date('05/01/2023', 'dd/mm/yyyy'), to_date('06/01/2028', 'dd/mm/yyyy'), 0);
-
--- prueba fecha_fin_profesor_investigacion
-insert into investigacion values ('INV-4', 'SUP-2', 'Investigacion 4', 'INV3', 'Descripcion 4', 'Financiador 4', 4000, to_date('01/01/2023', 'dd/mm/yyyy'), to_date('01/01/2024', 'dd/mm/yyyy'));
--- no pasa
--- trigger que ayude verificar que la fecha de inicio de la investigacion sea menor a la fecha de inicio pasada en profesor_investigacion
-insert into profesor_investigacion values ('PRF-4', 'INV-4', to_date('05/01/2023', 'dd/mm/yyyy'), to_date('06/01/2024', 'dd/mm/yyyy'), 0);
--- pasa
-insert into profesor_investigacion values ('PRF-4', 'INV-4', to_date('05/01/2023', 'dd/mm/yyyy'), to_date('06/01/2023', 'dd/mm/yyyy'), 0);
-
--- prueba fecha_inicio_profesor_investigacion
-insert into investigacion values ('INV-4', 'SUP-2', 'Investigacion 4', 'INV3', 'Descripcion 4', 'Financiador 4', 4000, to_date('01/01/2023', 'dd/mm/yyyy'), to_date('01/01/2024', 'dd/mm/yyyy'));
--- no pasa
-insert into profesor_investigacion values ('PRF-4', 'INV-4', to_date('05/01/2021', 'dd/mm/yyyy'), to_date('06/01/2023', 'dd/mm/yyyy'), 0);
--- pasa
-insert into profesor_investigacion values ('PRF-4', 'INV-4', to_date('05/01/2023', 'dd/mm/yyyy'), to_date('06/01/2023', 'dd/mm/yyyy'), 0);
-
--- prueba trigger participacion_profesor_investigacion
-insert into profesor values ('PRF-4', 'Damian', 'Nicolalde', 4, '123', 'Doctor');
-insert into investigacion values ('INV-3', 'Investigacion 3', 'INV3', 'Descripcion 3', 'Financiador 3', 3000, to_date('03/01/2023', 'dd/mm/yyyy'), to_date('03/01/2024', 'dd/mm/yyyy'));
-
--- primera participacion
-insert into profesor_investigacion values ('PRF-1', 'INV-3', to_date('03/01/2023', 'dd/mm/yyyy'), to_date('01/04/2023', 'dd/mm/yyyy'), 0);
--- segunda participacio
-insert into profesor_investigacion values ('PRF-1', 'INV-3', to_date('01/07/2023', 'dd/mm/yyyy'), to_date('01/09/2023', 'dd/mm/yyyy'), 0);
-insert into profesor_investigacion values ('PRF-1', 'INV-3', to_date('01/07/2023', 'dd/mm/yyyy'), to_date('01/09/2023', 'dd/mm/yyyy'), 0);
-insert into profesor_investigacion values ('PRF-1', 'INV-3', to_date('02/07/2023', 'dd/mm/yyyy'), to_date('03/07/2023', 'dd/mm/yyyy'), 0);
--- ultima y tercera
-insert into profesor_investigacion values ('PRF-1', 'INV-3', to_date('01/12/2023', 'dd/mm/yyyy'), to_date('01/01/2024', 'dd/mm/yyyy'), 0);
-
--- prueba trg_validar_publicacion
--- no pasa
-insert into publicacion values (3, 'INV-3', 'REV-2', 'CON-2', 'Titulo 3');
--- pasa
-insert into publicacion values (3, 'INV-3', NULL, 'CON-2', 'Titulo 3');
+insert into profesor_investigacion values ('PROF_002', 'INVE_005', to_date('01/01/2019', 'dd/mm/yyyy'), to_date('01/01/2020', 'dd/mm/yyyy'), 0);
