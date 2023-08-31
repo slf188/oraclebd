e0ff3482249ec1c1b7a03126e6775e4a12f1f0fb
菲利 <stephanovallejom@duck.com>
2023-08-31 14:01:19 -0500

    Update proced_aleatorio_6.sql

--- bd2/proced_aleatorio_6.sql
+++ bd2/proced_aleatorio_6.sql
@@ -1,5 +1,32 @@
 show user;
 
+sqlplus /nolog
+
+conn sys as sysdba
+
+CREATE TABLESPACE "TSTABLA"
+DATAFILE 'C:\Oracle\Database21c\oradata\ORCL\prueba.DBF' SIZE 100M;
+-- en mi computadora es C:\app\LuisVallejo\product\21c\oradata\XE\prueba.DBF
+-- podemos agrandar el tamano del tablespace con un update
+
+-- para crear un tablespace temporal lo hacemos de la siguiente manera
+CREATE TEMPORARY TABLESPACE "TSTABLA_TEMP" TEMPFILE 'C:\Oracle\Database21c\oradata\ORCL\prueba_TEMP.DBF'  SIZE 100M;
+
+alter session set "_ORACLE_SCRIPT"=true;
+
+create user usr_felipe
+identified by lticPUCE
+default tablespace TSTABLA quota unlimited on TSTABLA 
+temporary tablespace TSTABLA_TEMP;
+
+create role desarrollo;
+
+grant create session, create table, alter any table, drop any table,
+insert any table, update any table, delete any table, select any table
+to desarrollo;
+
+GRANT DESARROLLO TO USR_FELIPE;
+
 create table prueba (
     pru_id integer not null,
     pru_numero integer not null,
